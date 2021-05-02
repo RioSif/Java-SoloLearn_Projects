6ae560edbc6d90144ca1a33d15c5f97944d8479a
RioSif <riosif@gmail.com>
2021-05-02 03:39:10 +0300

    ✅

---
+++ Shapes/Shape.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+abstract class Shape {
+    int width;
+    abstract void area();
+}
+//your code goes here
+class Square extends Shape {
+
+    public Square(int width) {
+        this.width = width;
+    }
+
+    @Override
+    void area() {
+        System.out.println(width * width);
+    }
+}
+
+class Circle extends Shape {
+
+    public Circle(int width) {
+        this.width = width;
+    }
+
+    @Override
+    void area() {
+        System.out.println(Math.PI * width * width);
+    }
+}
+
+
